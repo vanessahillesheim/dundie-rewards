4c0b4a8708b8ba2fbb69dcf7ab38706c83c52d68
Vanessa Hillesheim <vanessaahillesheim@gmail.com>
2026-01-04 17:47:48 -0300

    Issue #10: Implementação do banco de dados
    - Adicionado módulo database.py com funções de conexão
    - Criado settings.py com configurações do projeto
    - Implementado sistema de usuários e autenticação
    - Adicionados testes para o banco de dados
    - Atualizadas dependências nos requirements
    - Reorganizada estrutura de arquivos de teste

---
+++ dundie/utils/email.py
@@ -0,0 +1,29 @@
+import re
+import smtplib
+from email.mime.text import MIMEText
+from dundie.settings import SMTP_HOST, SMTP_PORT, SMTP_TIMEOUT
+from dundie.utils.log import get_logger
+
+log = get_logger()
+
+regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z2-9.-]+\.[a-z]{2,}\b"
+
+def check_valid_email(address):
+    """Return true if email is valid"""
+    return bool(re.fullmatch(regex, address))
+
+def send_email(from_, to, subject, text):
+    if not isinstance(to, list):
+        #raise ValueError("To must be a list")
+        to = [to]
+
+    try:
+        with smtplib.SMTP(host = SMTP_HOST, port = SMTP_PORT, timeout=SMTP_TIMEOUT) as server:
+            message = MIMEText(text)
+            message["Subject"] = subject
+            message["From"] = from_
+            message["To"] = ",".join(to)
+            server.sendmail(from_, to, message.as_string())
+    
+    except Exception:
+        log.error("Cannot send email to %s", to)
